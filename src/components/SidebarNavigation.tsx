f6dccd875dd5106ce589aef51acec7d5da0413e6
MeowQuin <lcngmnhkhoi@gmail.com>
2023-04-28 23:09:22 +0700

    version 1.2

--- src/components/SidebarNavigation.tsx
+++ src/components/SidebarNavigation.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 import {
-    AppstoreOutlined,
+    FileOutlined,
     CalendarOutlined,
-    LinkOutlined,
+    SendOutlined,
     MailOutlined,
     LeftOutlined,
     RightOutlined,
+    PlusOutlined,
     SettingOutlined,
-    ArrowLeftOutlined,
-    ArrowRightOutlined
 } from '@ant-design/icons'
 import { Button, Divider, Menu, Switch } from 'antd'
 import type { MenuProps, MenuTheme } from 'antd/es/menu'
@@ -39,39 +38,13 @@ export default () => {
     }
 
     const items: MenuItem[] = [
-        {
-            key: 'collapse',
-            icon: collapse ? <RightOutlined /> : <LeftOutlined />,
-            onClick: toggleCollapse
-        },
-        getItem('Navigation One', '1', <MailOutlined />),
-        getItem('Navigation Two', '2', <CalendarOutlined />),
-        getItem('Navigation Two', 'sub1', <AppstoreOutlined />, [
-            getItem('Option 3', '3'),
-            getItem('Option 4', '4'),
-            getItem('Submenu', 'sub1-2', null, [
-                getItem('Option 5', '5'),
-                getItem('Option 6', '6')
-            ])
-        ]),
+        getItem('Inbox', '1', <MailOutlined />),
+        getItem('Starred', '2', <CalendarOutlined />),
+        getItem('Sent', '3', <SendOutlined />),
+        getItem('Drafts', '4', <FileOutlined />),
         { type: 'divider' },
-        getItem('Navigation Three', 'sub2', <SettingOutlined />, [
-            getItem('Option 7', '7'),
-            getItem('Option 8', '8'),
-            getItem('Option 9', '9'),
-            getItem('Option 10', '10')
-        ]),
-        getItem(
-            <a
-                href="https://ant.design"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Ant Design
-            </a>,
-            'link',
-            <LinkOutlined />
-        )
+        getItem('Manage labels', '5', <SettingOutlined />),
+        getItem('Create new label', '6', <PlusOutlined />),
     ]
 
     const siderStyle: React.CSSProperties = {
@@ -91,6 +64,9 @@ export default () => {
             onBreakpoint={(broken) => {
                 setCollapse(broken)
             }}
+            collapsible
+            onCollapse={(value) => setCollapse(value)}
+            trigger={collapse ? <RightOutlined /> : <LeftOutlined />}
         >
             <Menu
                 style={{ width: '100%', height: '100%' }}
