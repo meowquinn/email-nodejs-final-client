cac08186826b5b62c0e511dfa73e3478d2ea099b
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2023-05-02 13:08:14 +0700

    add profile button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UserOutlined } from '@ant-design/icons'
-import { AutoComplete, Input } from 'antd'
+import {
+    AutoComplete,
+    Avatar,
+    Button,
+    Descriptions,
+    Drawer,
+    Input,
+    Space
+} from 'antd'
 
 const renderTitle = (title: string) => (
     <span>
@@ -54,15 +62,59 @@ const options = [
     }
 ]
 
-export default () => (
-    <div className="flex h-full w-full flex-row items-center justify-center">
-        <AutoComplete
-            popupClassName="certain-category-search-dropdown"
-            dropdownMatchSelectWidth={500}
-            style={{ width: '100%' }}
-            options={options}
-        >
-            <Input.Search size="large" placeholder="Find email" />
-        </AutoComplete>
-    </div>
-)
+export default () => {
+    const [open, setOpen] = useState(false)
+    const showDrawer = () => {
+        setOpen(true)
+    }
+    const onClose = () => {
+        setOpen(false)
+    }
+
+    return (
+        <div className="flex h-full w-full flex-row items-center justify-around">
+            <AutoComplete
+                popupClassName="certain-category-search-dropdown"
+                dropdownMatchSelectWidth={500}
+                style={{ width: '60%' }}
+                options={options}
+            >
+                <Input.Search size="large" placeholder="Find email" />
+            </AutoComplete>
+            <Button type="dashed" size="large" onClick={showDrawer}>
+                Profile
+            </Button>
+            <Drawer
+                placement="right"
+                width={512}
+                onClose={onClose}
+                open={open}
+                extra={
+                    <Space>
+                        <Button onClick={onClose}>Cancel</Button>
+                        <Button type="primary" onClick={onClose}>
+                            OK
+                        </Button>
+                    </Space>
+                }
+            >
+                <Descriptions title="User Info">
+                    <Descriptions.Item label="UserName">
+                        Zhou Maomao
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Telephone">
+                        1810000000
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Live">
+                        Hangzhou, Zhejiang
+                    </Descriptions.Item>
+                    <Descriptions.Item label="Remark">empty</Descriptions.Item>
+                    <Descriptions.Item label="Address">
+                        No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang,
+                        China
+                    </Descriptions.Item>
+                </Descriptions>
+            </Drawer>
+        </div>
+    )
+}
